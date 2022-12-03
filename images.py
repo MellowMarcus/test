26543c29742f5c2578bfcd26fbb82f8715352ada
Mellow is fine <114035155+MellowMarcus@users.noreply.github.com>
2022-12-02 21:38:12 -0400

    Update images.py

--- images.py
+++ images.py
@@ -18,23 +18,12 @@ class LoginForm(FlaskForm):
 	submit = SubmitField("Submit")
 
 
-# Create a Posts Form
-class PostForm(FlaskForm):
-	title = StringField("Title", validators=[DataRequired()])
-	#content = StringField("Content", validators=[DataRequired()], widget=TextArea())
-	content = CKEditorField('Content', validators=[DataRequired()])
-	
-	#author = StringField("Author")
-	slug = StringField("Slug", validators=[DataRequired()])
-	submit = SubmitField("Submit")
 
 # Create a Form Class
 class UserForm(FlaskForm):
 	name = StringField("Name", validators=[DataRequired()])
 	username = StringField("Username", validators=[DataRequired()])
 	email = StringField("Email", validators=[DataRequired()])
-	favorite_color = StringField("Favorite Color")
-	about_author = TextAreaField("About Author")
 	password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='Passwords Must Match!')])
 	password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
 	profile_pic = FileField("Profile Pic")
