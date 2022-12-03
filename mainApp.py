77965443b18dc28cb9cc543b825be395bbc3c33a
Mellow is fine <114035155+MellowMarcus@users.noreply.github.com>
2022-12-02 21:56:47 -0400

    Update mainApp.py

--- mainApp.py
+++ mainApp.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash 
+from datetime import date
 
 # Create a Flask Instance
 app = Flask(__name__)
@@ -9,8 +13,39 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+#Views
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+	name = None
+	form = NamerForm()
+	# Validate Form
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+		flash("Form Submitted Successfully!")
+		
+	return render_template("name.html", 
+		name = name,
+		form = form)
 
-# Create Model
+
+
+@property
+def password(self):
+	raise AttributeError('password is not a readable attribute!')
+
+@password.setter
+def password(self, password):
+	self.password_hash = generate_password_hash(password)
+
+def verify_password(self, password):
+	return check_password_hash(self.password_hash, password)
+
+
+
+
+
+#Models
 class Users(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(20), nullable=False, unique=True)
@@ -20,4 +55,4 @@ class Users(db.Model, UserMixin):
 
 	# Do some password stuff!
 	password_hash = db.Column(db.String(128))
-	
+
