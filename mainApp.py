6614fa2b1bdfd01d46dd5f00fe8eacfdc0ee8d15
Mellow is fine <114035155+MellowMarcus@users.noreply.github.com>
2022-12-02 21:50:17 -0400

    Update mainApp.py

--- mainApp.py
+++ mainApp.py
@@ -3,5 +3,21 @@ from flask import Flask, render_template, flash, request, redirect, url_for
 # Create a Flask Instance
 app = Flask(__name__)
 
-
+app.config['SECRET_KEY'] = "secret hehehe"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+# Create Model
+class Users(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), nullable=False, unique=True)
+	name = db.Column(db.String(200), nullable=False)
+	date_added = db.Column(db.DateTime, default=datetime.utcnow)
+	profile_pic = db.Column(db.String(), nullable=True)
+
+	# Do some password stuff!
+	password_hash = db.Column(db.String(128))
+	
